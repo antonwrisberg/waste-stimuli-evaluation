639091269cb52cef94d778370df3a5d36eeecebe
Anton Wrisberg <anton@wrisberg.org>
2024-06-01 11:44:20 +0200

    Add attention checks to prototypicality and frequency setups

--- src/trials/prototypicality-free-sort-line-timer.js
+++ src/trials/prototypicality-free-sort-line-timer.js
@@ -152,12 +152,61 @@ export default function (experiment) {
             jsPsych.setProgressBar(0.05 + progressPercentageBasedOnTime * 0.9);
           }
         }
+      },
+      {
+        timeline: [{
+          type: HtmlButtonResponsePlugin,
+          stimulus: function() {
+            switch (experiment.detectLanguage()) {
+              case 'en':
+                return `
+                  <p>This screen is an attention check.</p>
+                  <p>Please do not click the button below.<br />The experiment will continue automatically in a few seconds.</p>
+                `;
+              case 'da':
+                return `
+                  <p>Dette skærmbillede er et opmærksomhedstjek.</p>
+                  <p>Vær venlig ikke at klikke på knappen nedenfor.<br />Eksperimentet fortsætter automatisk om nogle sekunder.</p>
+                `;
+              case 'sv':
+                return `
+                  <p>Denna skärmbild är en uppmärksamhetskontroll.</p>
+                  <p>Var vänlig och klicka inte på knappen nedan.<br />Experimentet fortsätter automatiskt om några sekunder.</p>
+                `;
+              default:
+                break;
+            }
+          },
+          choices: function() {
+            switch (experiment.detectLanguage()) {
+              case 'en':
+                return ["Continue"];
+              case 'da':
+                return ["Fortsæt"];
+              case 'sv':
+                return ["Fortsätt"];
+              default:
+                break;
+            }
+          },
+          on_finish: function(data) {
+            if (data.response === 0) {
+              data.attention_check_response = 'Failed attention check! This button should not have been clicked';
+            } else {
+              data.attention_check_response = 'Successful attention check!';
+            }
+          },
+          trial_duration: 6000
+        }],
+        conditional_function: function(){
+          return (Math.random() > .6)
+        }
       }
     ],
     timeline_variables: experiment.stimuli.fractions,
     sample: {
       type: 'with-replacement',
-      size: 24,
+      size: 36,
       weights: experiment.stimuli.fractionCounts
     }
   });
