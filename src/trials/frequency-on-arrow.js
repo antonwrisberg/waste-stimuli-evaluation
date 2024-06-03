817ab134416f7f49c6d447a46c4716e1049c5a7a
Anton Wrisberg <anton@wrisberg.org>
2024-06-03 18:16:54 +0200

    Remove unused shuffle function

--- src/trials/frequency-on-arrow.js
+++ src/trials/frequency-on-arrow.js
@@ -1,21 +1,6 @@
 import HtmlButtonResponsePlugin from "@jspsych/plugin-html-button-response";
 import ImagesLineSortPlugin from "/node_modules/@antonwrisberg/plugin-images-freesort";
 
-
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) { 
- 
-      // Generate random number 
-      var j = Math.floor(Math.random() * (i + 1));
-                 
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-  }
-     
-  return array;
-}
-
 // Prototypicality with free sort
 export default function (experiment) {
   jsPsych = experiment.jsPsych;
